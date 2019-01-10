1358fa16f2a4f8003e4ab32cd49b9eae58d5ffc4
clintonpi <nkwochaclinton@gmail.com>
2019-01-10 21:27:24 +0100

    Test: Add test for getting todos

--- server/todos/todo.spec.js
+++ server/todos/todo.spec.js
@@ -158,4 +158,96 @@ describe('Todo Actions', () => {
         });
     });
   });
+
+  describe('Get Todos', () => {
+    it('should fail to return the todo(s) if "authorization" is not set in the request header', (done) => {
+      chai.request(app)
+        .get('/todo')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to return the todo(s) if "authorization" in the req header is invalid', (done) => {
+      chai.request(app)
+        .get('/todo')
+        .set('authorization', 'Bearer')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to return the todo(s) if the authorization token is invalid', (done) => {
+      chai.request(app)
+        .get('/todo')
+        .set('authorization', 'Bearer invalidToken')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to return the todo(s) if the user id generated from the authorization token does not exist', (done) => {
+      chai.request(app)
+        .get('/todo')
+        .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjZmJjYjIwLTA4ZTEtMTFlOS05MWZlLTlmMzg0M2Q5NDdmZCIsInVzZXJuYW1lIjoiY2xpbnRvbmFtZSIsImVtYWlsIjoibmt3b2NoYWNsaW50b25AZ21haWwuY29tYW1lIiwiaWF0IjoxNTQ1ODA5OTYzfQ.PhMNI57KYploKDfLqdx0Coije0mNaNq_5eBb7AVQkyI')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should not return any todo if the user does not have any', (done) => {
+      chai.request(app)
+        .post('/signup')
+        .send({
+          username: 'animal', email: 'animal@being.com', password: 'animalbeing', password2: 'animalbeing'
+        })
+        .end((err, res) => {
+          chai.request(app)
+            .get('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(200);
+              expect(res.body.message).to.be.eq('You have no todo.');
+              done();
+            });
+        });
+    });
+
+    it('should return all the todos for the user', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          const { token } = res.body;
+          chai.request(app)
+            .post('/todo')
+            .set('authorization', `Bearer ${token}`)
+            .send({ todoName: 'Write unit tests' })
+            .end(() => {
+              chai.request(app)
+                .get('/todo')
+                .set('authorization', `Bearer ${token}`)
+                .end((err, res) => {
+                  expect(err).to.be.null;
+                  expect(res).to.have.status(200);
+                  expect(res.body).to.have.property('todos');
+                  done();
+                });
+            });
+        });
+    });
+  });
 });
