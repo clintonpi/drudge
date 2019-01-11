7745faaffe023bd4b9db37a522585183bbb9d009
clintonpi <nkwochaclinton@gmail.com>
2019-01-11 12:17:35 +0100

    Test: Add tests for deleting of todos

--- server/todos/todo.spec.js
+++ server/todos/todo.spec.js
@@ -486,4 +486,235 @@ describe('Todo Actions', () => {
         });
     });
   });
+
+  describe('Delete Todo', () => {
+    let todosId;
+
+    beforeEach((done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .get('/todo')
+            .set('authorization', `Bearer ${res.body.token}`)
+            .end((err, res) => {
+              todosId = [];
+              res.body.todos.forEach((todo) => {
+                todosId.push(todo.id);
+              });
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete the todo(s) if "authorization" is not set in the request header', (done) => {
+      chai.request(app)
+        .delete('/todo')
+        .send({ todosId })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to delete the todo(s) if "authorization" in the req header is invalid', (done) => {
+      chai.request(app)
+        .delete('/todo')
+        .set('authorization', 'Bearer')
+        .send({ todosId })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to delete the todo(s) if the authorization token is invalid', (done) => {
+      chai.request(app)
+        .delete('/todo')
+        .set('authorization', 'Bearer invalidToken')
+        .send({ todosId })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to delete the todo(s) if the user id generated from the authorization token does not exist', (done) => {
+      chai.request(app)
+        .delete('/todo')
+        .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjZmJjYjIwLTA4ZTEtMTFlOS05MWZlLTlmMzg0M2Q5NDdmZCIsInVzZXJuYW1lIjoiY2xpbnRvbmFtZSIsImVtYWlsIjoibmt3b2NoYWNsaW50b25AZ21haWwuY29tYW1lIiwiaWF0IjoxNTQ1ODA5OTYzfQ.PhMNI57KYploKDfLqdx0Coije0mNaNq_5eBb7AVQkyI')
+        .send({ todosId })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to delete the todo(s) if "todosId" was not sent', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was incomplete.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete the todo(s) if "todosId" was not an array', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId: 1 })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was invalid.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete the todo(s) if the length of "todosId" array is less than one', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId: [] })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was invalid.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete a todo if it does not exist', (done) => {
+      todosId = ['0105ded0-1536-11e9-a23f-57af3506ee08'];
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('This todo does not exist.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete the todos if any of them does not exist', (done) => {
+      todosId.push('0105ded0-1536-11e9-a23f-57af3506ee08');
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Some of these todos do not exist.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete a todo if it was not created by this user', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'animal@being.com', password: 'animalbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('You are not the owner of this todo.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to delete the todos if any of them was not created by this user', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .post('/todo') // create one more todo
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todoName: 'Do something else' })
+            .end((err, res) => {
+              const { todoId } = res.body;
+              todosId.push(todoId);
+              // add the gotten todo id to todosId to make the length greater than one
+              chai.request(app)
+                .post('/login') // login as another user
+                .send({ email: 'animal@being.com', password: 'animalbeing' })
+                .end((err, res) => {
+                  chai.request(app)
+                    .delete('/todo')
+                    .set('authorization', `Bearer ${res.body.token}`) // get valid token
+                    .send({ todosId })
+                    .end((err, res) => {
+                      expect(err).to.be.null;
+                      expect(res).to.have.status(400);
+                      expect(res.body.message).to.be.eq('You are not the owner of some of these todos.');
+                      done();
+                    });
+                });
+            });
+        });
+    });
+
+    it('should delete the todo(s)', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          chai.request(app)
+            .delete('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send({ todosId })
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
 });
