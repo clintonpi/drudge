3288e55100914ed203804b24329f27b5eae92ec0
clintonpi <nkwochaclinton@gmail.com>
2019-01-11 08:23:48 +0100

    Test: Add tests for updating a todo

--- server/todos/todo.spec.js
+++ server/todos/todo.spec.js
@@ -244,4 +244,246 @@ describe('Todo Actions', () => {
         });
     });
   });
+
+  describe('Update Todo', () => {
+    let todo;
+
+    beforeEach((done) => {
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .get('/todo')
+            .set('authorization', `Bearer ${res.body.token}`)
+            .end((err, res) => {
+              const firstTodo = res.body.todos[0];
+              todo = {
+                todoId: firstTodo.id,
+                todoName: firstTodo.name,
+                isDone: firstTodo.done
+              }
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if "authorization" is not set in the request header', (done) => {
+      chai.request(app)
+        .put('/todo')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to update the todo if "authorization" in the req header is invalid', (done) => {
+      chai.request(app)
+        .put('/todo')
+        .set('authorization', 'Bearer')
+        .send(todo)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to update the todo if the authorization token is invalid', (done) => {
+      chai.request(app)
+        .put('/todo')
+        .set('authorization', 'Bearer invalidToken')
+        .send(todo)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to update the todo if the user id generated from the authorization token does not exist', (done) => {
+      chai.request(app)
+        .put('/todo')
+        .set('authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjZmJjYjIwLTA4ZTEtMTFlOS05MWZlLTlmMzg0M2Q5NDdmZCIsInVzZXJuYW1lIjoiY2xpbnRvbmFtZSIsImVtYWlsIjoibmt3b2NoYWNsaW50b25AZ21haWwuY29tYW1lIiwiaWF0IjoxNTQ1ODA5OTYzfQ.PhMNI57KYploKDfLqdx0Coije0mNaNq_5eBb7AVQkyI')
+        .send(todo)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.redirect;
+          expect(res.req.path).to.be.eq('/login');
+          done();
+        });
+    });
+
+    it('should fail to update the todo if "todoName" was not sent', (done) => {
+      todo.todoName = undefined;
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was incomplete.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if "todoName" was not not a string', (done) => {
+      todo.todoName = 1;
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your todo was invalid.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if the length of the sanitized todoname is less than one', (done) => {
+      todo.todoName = '      ';
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your todo was invalid.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if "todoId" was not sent', (done) => {
+      todo.todoId = undefined;
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was incomplete.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if "isDone" was not sent', (done) => {
+      todo.isDone = undefined;
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your request was incomplete.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if "isDone" is not a boolean', (done) => {
+      todo.isDone = 'invalidStatus';
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('Your todo status was invalid.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if it does not exist', (done) => {
+      todo.todoId = '0105ded0-1536-11e9-a23f-57af3506ee08'; // Valid uuid
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('This todo does not exist.');
+              done();
+            });
+        });
+    });
+
+    it('should fail to update the todo if it was not created by this user', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'animal@being.com', password: 'animalbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(400);
+              expect(res.body.message).to.be.eq('You are not the owner of this todo.');
+              done();
+            });
+        });
+    });
+
+    it('should update the todo', (done) => {
+      chai.request(app)
+        .post('/login')
+        .send({ email: 'human@being.com', password: 'humanbeing' })
+        .end((err, res) => {
+          chai.request(app)
+            .put('/todo')
+            .set('authorization', `Bearer ${res.body.token}`) // get valid token
+            .send(todo)
+            .end((err, res) => {
+              expect(err).to.be.null;
+              expect(res).to.have.status(200);
+              done();
+            });
+        });
+    });
+  });
 });
